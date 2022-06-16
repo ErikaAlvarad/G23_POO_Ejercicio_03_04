c957ccf004fd49976f4155dfc98f78a87a21de2b
erika <erika@DESKTOP-BIU9HD7>
2022-06-16 08:46:51 -0500

    Trabajo Finalizado

---
+++ src/main/java/vista/G23_POO_Ejercicio_03_04.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package vista;
+
+/**
+ *
+ * @author erika
+ */
+public class G23_POO_Ejercicio_03_04 {
+
+    public static void main(String[] args) {
+        var v1 = new ArticuloVentana();
+        var v2 = new AutorPrincipalVentana();
+        var v3 = new RevistaVentana();
+        var v4 = new VentanaEliminar();
+        var v5 = new VentanaModificar();
+        
+        v1.setVisible(true);
+        v2.setVisible(true);
+        v3.setVisible(true);
+        v4.setVisible(true);
+        v5.setVisible(true);
+        
+    }
+}
